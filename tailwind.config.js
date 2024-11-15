e54bc6b4604990bb44c094e262f6ae7f1a8bb739
hasan-iftakhar <iftakharhasan46@gmail.com>
2024-11-16 01:11:09 +0600

    my tailwind css project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['*'],
+  theme: {
+    extend: {
+      "fontFamily":{
+        'Roboto':['"Roboto", serif']
+      }
+    },
+  },
+
+
+  plugins: [],
+}
+
+
+
